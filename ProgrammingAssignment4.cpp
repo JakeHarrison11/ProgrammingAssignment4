d210d3c46c813709b02ef13a4f8052a2995c181e
Jake Harrison <harrisonjt1@mail.irsc.edu>
2022-03-03 00:15:50 -0500

    bonk

--- ProgrammingAssignment4.cpp
+++ ProgrammingAssignment4.cpp
@@ -1,20 +1,35 @@
-// ProgrammingAssignment4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int option;
+    double p = 0.0;
+    const double GOLD = 0.01;
+    const double SILVER = 0.02;
+    const double BRONZE = 0.04;
+    double BASEFEE = 1200.0;
+
+    cout << "Rhoda's Strikeforce Gym\n";
+
+    cout << "Option 1-4: ";
+    cin >> option;
+    switch (option) {
+    case 1:
+        p = GOLD;
+        break;
+    case 2:
+        p = SILVER;
+        break;
+    case 3:
+        p = BRONZE;
+        break;
+    case 4:
+        cout << "bye bitch";
+        exit(0);
+    }
+    for (double i = 1.0; i <= 10.0; i += 1.0) {
+        cout << "Year " << i << "    " << p * (i - 1.0) * BASEFEE + BASEFEE << endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
